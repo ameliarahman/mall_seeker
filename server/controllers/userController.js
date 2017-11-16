4d52497349f4b0ceaa075a3353c6499fd6a8ce4c
ameliarahman <amel.rahman5@gmail.com>
2017-11-16 20:44:35 +0700

    Add jquery geolocation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,41 +6,41 @@ require('dotenv').config()
 function checkDatabase(req, res) {
   User.findOne({
     //find berdasarkan fbid
-    fbid : req.headers.fbid
+    fbid: req.headers.fbid
   })
-  //kalo error, promise coba ganti callback
-  .then(user => {
-    //kalo user == kosong, bikin baru, kalo tidak kosong, lanjut bikinin token
-    if (user === null) {
-      let newUser = new User({
-        // name :
-        // profilePicture :
-        // fbid :
-      })
-      newUser.save()
-      .then(success => {
-        //kalo ga di send, mungkin bikin jwt langsung di bawah
-        res.send(success)
-      })
-    }
-    jwt.sign({
-      name : user.name,
-      profilePicture : user.profilePicture,
-      fbid : user.fbid
-    },process.env.JWT_SECRET, (err, token) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send(err)
+    //kalo error, promise coba ganti callback
+    .then(user => {
+      //kalo user == kosong, bikin baru, kalo tidak kosong, lanjut bikinin token
+      if (user === null) {
+        let newUser = new User({
+          // name :
+          // profilePicture :
+          // fbid :
+        })
+        newUser.save()
+          .then(success => {
+            //kalo ga di send, mungkin bikin jwt langsung di bawah
+            res.send(success)
+          })
       }
-      else {
-        res.send(token)
-      }
+      jwt.sign({
+        name: user.name,
+        profilePicture: user.profilePicture,
+        fbid: user.fbid
+      }, process.env.JWT_SECRET, (err, token) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send(err)
+        }
+        else {
+          res.send(token)
+        }
+      })
     })
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).send(err)
-  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err)
+    })
 }
 
 module.exports = {
